d47f16663d459d5e552b74e5860a700252c2acc1
anthony ng pan <ngpananthony@gmail.com>
2022-06-13 14:04:44 +1100

    base

---
+++ src/main/java/nc/univ/edt/EdtApplication.java
@@ -0,0 +1,13 @@
+package nc.univ.edt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nc/univ/edt/EdtApplicationTests.java
@@ -0,0 +1,13 @@
+package nc.univ.edt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
