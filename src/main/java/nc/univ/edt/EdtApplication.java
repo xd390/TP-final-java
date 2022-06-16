03af0fc70fce71a026f01f21c80a9ce7a28d60a2
The_Phoenix78 <julien.lcq@icloud.com>
2022-06-16 15:17:23 +1100

    restructuration du projet pour view (vue) vers sources et mise en place de table html

--- src/main/java/nc/univ/edt/EdtApplication.java
+++ src/main/java/nc/univ/edt/EdtApplication.java
@@ -23,13 +23,13 @@ public class EdtApplication {
 	private static void printDatabaseDetail(ApplicationContext applicationContext) throws SQLException {
 
 		// Recuperation de la dataSource
-		DataSource dataSource = (DataSource)applicationContext.getBean("dataSource");
+		DataSource dataSource = (DataSource) applicationContext.getBean("dataSource");
 		Connection connection = dataSource.getConnection();
 		Statement statement = connection.createStatement();
 
 		// Recuperation et affichage du detail
 		ResultSet resultSetTables = statement.executeQuery("SHOW TABLES");
-		while(resultSetTables.next()) {
+		while (resultSetTables.next()) {
 
 			// Recuperation du nom de la table
 			String tableName = resultSetTables.getString(1);
@@ -37,7 +37,7 @@ public class EdtApplication {
 
 			Statement tableStatement = connection.createStatement();
 			ResultSet resultSet = tableStatement.executeQuery("select column_name, type_name from information_schema.columns where table_name='" + tableName + "'");
-			while(resultSet.next())
+			while (resultSet.next())
 				System.out.println("Colonne " + resultSet.getString(1) + " de type " + resultSet.getString(2));
 		}
 	}
--- src/main/java/nc/univ/edt/controller/ControllerApp.java
+++ src/main/java/nc/univ/edt/controller/ControllerApp.java
@@ -1,28 +1,41 @@
 package nc.univ.edt.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class ControllerApp {
 
     @RequestMapping(value = "consultation/salle", method = {RequestMethod.GET})
     @ResponseBody
-    public String consultationSalle(@RequestParam String salle){
+    public String consultationSalle(@RequestParam String salle, Model model){
+        String salleName = "A10";
+        model.addAttribute("salle", salleName);
+        return salle;
+    }
+
+    @GetMapping(value = "consultation/salle/{salle}")
+    @ResponseBody
+    public String consultationSalleStatic(@RequestParam String salle){
         return salle;
     }
 
     @GetMapping("form")
-    @ResponseBody
     public String salleForm(){
-        String html = "<html>"+
-                "<body>" +
-                "<form method = 'post' action = '/hello'>" +
-                "<input type = 'text' name = 'name' />" +
-                "<input type = 'submit' name = 'Salle!' />" +
-                "</form>" +
-                "</body>" +
-                "</html>";
-        return html;
+        return "form";
+    }
+
+    @GetMapping("cours/consultation")
+    public String list(Model model){
+        List<String> salles = new ArrayList<String>();
+        salles.add("A5");
+        salles.add("B10");
+        salles.add("C3");
+        model.addAttribute("salles", salles);
+        return "view/cours/consultationCours";
     }
 
 
