f5edaf4d8193b024f12f7a90028672d717013a08
The_Phoenix78 <julien.lcq@icloud.com>
2022-06-16 17:28:38 +1100

    Mise a jour des pages html, suppression de code inutile dans ControllerApp

--- src/main/java/nc/univ/edt/controller/ControllerApp.java
+++ src/main/java/nc/univ/edt/controller/ControllerApp.java
@@ -27,21 +27,10 @@ public class ControllerApp {
     @Autowired
     ApplicationContext applicationContext;
 
-    @RequestMapping(value = "consultation/salle", method = {RequestMethod.GET})
-    public String consultationSalle(@RequestParam String salle, Model model){
-        String salleName = "A10";
-        model.addAttribute("salle", salleName);
-        return salle;
-    }
 
     @GetMapping(value = "consultation/salle/{salle}")
     public String consultationSalleStatic(@RequestParam String salle){
         return salle;
-    }
-
-    @GetMapping("form")
-    public String salleForm(){
-        return "form";
     }
 
     @GetMapping("cours")
@@ -50,12 +39,10 @@ public class ControllerApp {
         model.addAttribute("cours", cours);
         return "view/cours/consultationCours";
     }
-
     @GetMapping("/cours/creation")
     public String CreationCours(){
         return "creationCours";
     }
-
     @PostMapping("/cours/creation")
     public Cours creationCours(@RequestBody Cours cours){
         return coursService.save(cours,applicationContext);
@@ -67,7 +54,6 @@ public class ControllerApp {
         model.addAttribute("eleves",eleves);
         return "consultationEleve";
     }
-
     @GetMapping("/eleve/creation")
     public String creationEleve(){
         return "creationEleve";
@@ -83,14 +69,12 @@ public class ControllerApp {
         model.addAttribute("salles",salles);
         return "consultationSalle";
     }
-
     @DeleteMapping("/salle")
     public Map<String, Boolean> suppressionSalle(@PathVariable("id") Long salleId ){
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted",salleService.delete(salleId,applicationContext));
         return response;
     }
-
     @GetMapping("/salle/modification")
     public String modificationSalle(@PathVariable("id") Long salleId,Model model){
         model.addAttribute("salle",salleService.get(salleId,applicationContext));
@@ -100,7 +84,6 @@ public class ControllerApp {
     public String creationSalle(){
         return "creationSalle";
     }
-
     @PostMapping("/salle/creation")
     public Salle creationSalle(@RequestBody Salle salle) {
         return salleService.save(salle,applicationContext);
